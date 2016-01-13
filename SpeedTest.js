84287fca325e3581ea3ea4a16fd8a8bda98c442b
Michel Llorens <mllorens@dcc.uchile.cl>
2016-01-13 15:58:08 -0300

    Added JSDoc

--- SpeedTest.js
+++ SpeedTest.js
@@ -1,9 +1,26 @@
+/**
+* @author Michel Llorens http://michotastico.github.io
+* @version 1.0.0
+*/
+
+/**
+ * SpeedTest - Class in charge of make Speed Tests
+ * @class speed_test
+ * @classdesc Class in charge of make Speed Tests
+ *
+ * @param  {string} file_url URL of the file used in the download speed test
+ */
 function SpeedTest(file_url){
   this.downloadSpeed = 0;
   this.testTime = 1;
   this.testResult = null;
   this.file = file_url;
 
+  /**
+   * getDownloadSpeed - Get the download speed
+   *
+   * @return {number}  download speed
+   */
   this.getDownloadSpeed = function(){
     if(this.downloadSpeed === 0){
       var _MB = this.testResult.dataSizeKB/1000;
@@ -13,26 +30,57 @@ function SpeedTest(file_url){
     return this.downloadSpeed;
   }
 
+  /**
+   * getLatency - Get the latency
+   *
+   * @return {number}  latency
+   */
   this.getLatency = function(){
     return this.testResult.latency;
   }
 
+  /**
+   * getLatencyType - Get the equivalence of latency
+   *
+   * @return {string}  Latency speed class name
+   */
   this.getLatencyType = function(){
     return this.testResult.latencySpeedClass.name;
   }
 
+  /**
+   * getThroughput - Get the throughput
+   *
+   * @return {number}  throughput
+   */
   this.getThroughput = function(){
     return this.testResult.throughput;
   }
 
+  /**
+   * getThroughputType - Get the equivalenceof throughput
+   *
+   * @return {string}  Throughput speed class name
+   */
   this.getThroughputType = function(){
     return this.testResult.throughPutSpeedClass.name;
   }
 
+  /**
+   * getTestTime - Get the total time of the test
+   *
+   * @return {number}  duration of test
+   */
   this.getTestTime = function(){
     return this.testTime;
   }
 
+  /**
+   * startSpeedTest - start the speed test
+   *
+   * @param  {function} onprogress on progress function
+   * @return {undefined} 
+   */
   this.startSpeedTest = function(onprogress){
     var selfTest = this;
     callback = function(timings){
