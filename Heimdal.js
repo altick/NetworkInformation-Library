9bfe378ef938f7624df7f793f9b3b47c783d6403
Michel Llorens <mllorens@dcc.uchile.cl>
2016-01-18 12:09:40 -0300

    Added time when send

--- Heimdal.js
+++ Heimdal.js
@@ -17,6 +17,7 @@ function Heimdal(server){
   this.server_url = server || 'http://localhost/';
   this.network_info = new NetworkInformation();
   this.speed_test = [];
+  this.time = Date.Now();
 
 
   /**
@@ -54,6 +55,7 @@ function Heimdal(server){
    * @return {undefined}
    */
   this.sendData = function(){
+    this.time = Date.Now();
     var information = JSON.stringify(this);
     $.ajax({
       type: 'POST',
